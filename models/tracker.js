8397b04e945a60dacfe9d91571c2c80fa75d0724
Aamir-pathan-ekcs <maakhan@ekcs.co>
2025-07-29 21:00:50 +0530

    minor changes reverse code

--- models/tracker.js
+++ models/tracker.js
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 
 const trackerData = new mongoose.Schema({
-    clickTime: {
-        type: String,
-        default: () => new Date().toISOString().split('T')[0]
-    },
+    loopCount: Number,
+    adhesion: Number,
     advertiserID: String,
     orderID: String,
     lineItemID: String,
     creativeID:  String,
-    loopCount: Number,
     video_db: {
         firstQuarter: {type: Number, default: 0},
         secondQuarter: {type: Number, default: 0},
         thirdQuarter: {type: Number, default: 0},
         fourthQuarter: {type: Number, default: 0}
     },
-    expand: Number
+    clickTime: {
+        type: String,
+        default: () => new Date().toISOString().split('T')[0]
+    }
 });
 
 export default mongoose.model('IQ', trackerData);
