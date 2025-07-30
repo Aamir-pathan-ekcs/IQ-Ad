78ee268bc74facb82f68e10c65a0db2a623bc773
Aamir-pathan-ekcs <maakhan@ekcs.co>
2025-07-31 01:09:15 +0530

    testing beac final last insh reverse

--- models/tracker.js
+++ models/tracker.js
@@ -1,19 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const trackerSchema = new mongoose.Schema({
+const trackerData = new mongoose.Schema({
+    loopCount: Number,
+    adhesion: Number,
     advertiserID: String,
     orderID: String,
     lineItemID: String,
-    creativeID: String,
-    loopCount: Number,
-    adhesion: Number,
+    creativeID:  String,
     video_db: {
-        firstQuarter: Number,
-        secondQuarter: Number,
-        thirdQuarter: Number,
-        fourthQuarter: Number
+        firstQuarter: {type: Number, default: 0},
+        secondQuarter: {type: Number, default: 0},
+        thirdQuarter: {type: Number, default: 0},
+        fourthQuarter: {type: Number, default: 0}
     },
-    // other fields as needed
+    clickTime: {
+        type: String,
+        default: () => new Date().toISOString().split('T')[0]
+    }
 });
 
-export default mongoose.model('tracker', trackerSchema);
+export default mongoose.model('IQ', trackerData);
