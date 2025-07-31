6c9a0dc19561892bb3eb4153520ac9551d7c6607
Aamir-pathan-ekcs <maakhan@ekcs.co>
2025-07-31 18:37:04 +0530

    covert to mysql changes more add ip change

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('admin_newsQuest_track', 'newsQuest_track', 'newsQuest_track@@321', {
   host: '165.22.213.254:8443', // or your remote host
+  port: 3306,
   dialect: 'mysql',
+  logging: false,
   logging: false, // optional
 });
 
